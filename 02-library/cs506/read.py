0979f5f4eb3937ee33ab056b22855eec975a9daa
Weihan Liu <mrwickham97@gmail.com>
2020-02-05 20:30:02 -0500

    implement read and sim

--- 02-library/cs506/read.py
+++ 02-library/cs506/read.py
@@ -1,6 +1,15 @@
 def read_csv(csv_file_path):
+    import csv
     """
         Given a path to a csv file, return a matrix (list of lists)
         in row major.
     """
-    raise NotImplementedError()
+    # raise NotImplementedError()
+    result = []
+    with open(csv_file_path) as csv_file:
+        csv_reader = csv.reader(csv_file)
+        for row in csv_reader:
+            row = [int(i) for i in row]
+            result.append(row)
+
+    return result
--- 02-library/cs506/sim.py
+++ 02-library/cs506/sim.py
@@ -1,13 +1,26 @@
+import math
+
 def euclidean_dist(x, y):
-    raise NotImplementedError()
+    return math.sqrt(sum([(a - b) ** 2 for a, b in zip(x, y)]))
+    # raise NotImplementedError()
 
 def manhattan_dist(x, y):
-    raise NotImplementedError()
+    return sum(abs(a-b) for a,b in zip(x,y))
+    # raise NotImplementedError()
 
 def jaccard_dist(x, y):
-    raise NotImplementedError()
+    intersection_cardinality = len(set.intersection(*[set(x), set(y)]))
+    union_cardinality = len(set.union(*[set(x), set(y)]))
+    return 1 - intersection_cardinality/float(union_cardinality)
+    # raise NotImplementedError()
 
 def cosine_sim(x, y):
-    raise NotImplementedError()
+    def square_rooted(x):
+        return round(math.sqrt(sum([a*a for a in x])),3)
+
+    numerator = sum(a*b for a,b in zip(x,y))
+    denominator = square_rooted(x)*square_rooted(y)
+    return round(numerator/float(denominator),3)
+    # raise NotImplementedError()
 
 # Feel free to add more
