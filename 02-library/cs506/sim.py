68081ce70fb9c15f18bc3cd9a12c3e627bbc4afe
Weihan Liu <mrwickham97@gmail.com>
2020-02-21 10:51:43 -0500

    local commit

--- 02-library/cs506/sim.py
+++ 02-library/cs506/sim.py
@@ -2,7 +2,6 @@ import math
 
 # def euclidean_dist(x, y):
 #     return math.sqrt(sum([(a - b) ** 2 for a, b in zip(x, y)]))
-#     # raise NotImplementedError()
 
 #     '''
 #     if len(x) != len(y):
@@ -15,22 +14,19 @@ import math
 
 # def manhattan_dist(x, y):
 #     return sum(abs(a-b) for a,b in zip(x,y))
-#     # raise NotImplementedError()
 
-# def jaccard_dist(x, y):
-#     intersection_cardinality = len(set.intersection(*[set(x), set(y)]))
-#     union_cardinality = len(set.union(*[set(x), set(y)]))
-#     return 1 - intersection_cardinality/float(union_cardinality)
-#     # raise NotImplementedError()
+# # def jaccard_dist(x, y):
+# #     intersection_cardinality = len(set.intersection(*[set(x), set(y)]))
+# #     union_cardinality = len(set.union(*[set(x), set(y)]))
+# #     return 1 - intersection_cardinality/float(union_cardinality)
 
-# def cosine_sim(x, y):
-#     def square_rooted(x):
-#         return round(math.sqrt(sum([a*a for a in x])),3)
+# # def cosine_sim(x, y):
+# #     def square_rooted(x):
+# #         return round(math.sqrt(sum([a*a for a in x])),3)
 
-#     numerator = sum(a*b for a,b in zip(x,y))
-#     denominator = square_rooted(x)*square_rooted(y)
-#     return round(numerator/float(denominator),3)
-#     # raise NotImplementedError()
+# #     numerator = sum(a*b for a,b in zip(x,y))
+# #     denominator = square_rooted(x)*square_rooted(y)
+# #     return round(numerator/float(denominator),3)
 
 # # Feel free to add more
 
--- 02-library/tests/test_kmeans.py
+++ 02-library/tests/test_kmeans.py
@@ -21,7 +21,7 @@ def test_kmeans_when_k_is_1(datasetPath):
     #random.seed(1)
     dataset = read.read_csv(datasetPath)
     expected_clustering = dataset
-    clustering = kmeans.k_means_pp(dataset=dataset, k=1)
+    clustering = kmeans.k_means(dataset=dataset, k=1)
 
     assert len(clustering.keys()) == 1
     assert clustered_all_points(clustering, dataset) is True
@@ -38,15 +38,15 @@ def test_kmeans_when_k_is_1(datasetPath):
      "tests/test_files/dataset_1_k_is_2_1.csv"),
 ])
 def test_kmeans_when_k_is_2(datasetPath, expected1, expected2):
-    # random.seed(1)
+    random.seed(1)
     dataset = read.read_csv(datasetPath)
     expected_clustering1 = read.read_csv(expected1)
     expected_clustering2 = read.read_csv(expected2)
-    clustering = kmeans.k_means_pp(dataset=dataset, k=2)
+    clustering = kmeans.k_means(dataset=dataset, k=2)
     cost = kmeans.cost_function(clustering)
 
-    for _ in range(800):
-        new_clustering = kmeans.k_means_pp(dataset=dataset, k=2)
+    for _ in range(10):
+        new_clustering = kmeans.k_means(dataset=dataset, k=2)
         new_cost = kmeans.cost_function(clustering)
         if new_cost < cost:
             clustering = new_clustering
@@ -68,16 +68,16 @@ def test_kmeans_when_k_is_2(datasetPath, expected1, expected2):
      "tests/test_files/dataset_1_k_is_3_2.csv"),
 ])
 def test_kmeans_when_k_is_3(datasetPath, expected1, expected2, expected3):
-    # random.seed(1)
+    random.seed(1)
     dataset = read.read_csv(datasetPath)
     expected_clustering1 = read.read_csv(expected1)
     expected_clustering2 = read.read_csv(expected2)
     expected_clustering3 = read.read_csv(expected3)
-    clustering = kmeans.k_means_pp(dataset=dataset, k=3)
+    clustering = kmeans.k_means(dataset=dataset, k=3)
     cost = kmeans.cost_function(clustering)
 
-    for _ in range(600):
-        new_clustering = kmeans.k_means_pp(dataset=dataset, k=3)
+    for _ in range(10):
+        new_clustering = kmeans.k_means(dataset=dataset, k=3)
         new_cost = kmeans.cost_function(clustering)
         if new_cost < cost:
             clustering = new_clustering
